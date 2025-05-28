385110c066b834b78e63cc33e73266be020a18a9
NickiRimmer <160492646+NickiRimmer@users.noreply.github.com>
2025-05-28 16:32:57 +0300

    remove sth

--- modules/index.php
+++ modules/index.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 header('Content-Type: text/html; charset=UTF-8');
-echo "<link rel='stylesheet' href='style.css'>";
 
 $allowed_includes = ['modules/db.php', 'modules/validation.php'];
 
